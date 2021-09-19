fe861e9cc8d771034248783913f9fee3b04cec29
mozzy11 <mozzymutesa@gmail.com>
2021-09-19 14:51:31 +0300

    inntial qaframework  structure

---
+++ src/test/java/org/itech/locatorform/qaframework/automation/page/HealthDeskViewPage.java
@@ -0,0 +1,17 @@
+package org.itech.locatorform.qaframework.automation.page;
+
+
+public class HealthDeskViewPage extends Page{
+
+    public HealthDeskViewPage(Page page) {
+        super(page);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public String getPageUrl() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
